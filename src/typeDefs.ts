80f5591b1238d017a46ab5bdef04feb8e091cbbe
Darrell Warde <darrell.warde@neotechnology.com>
2021-06-16 18:28:29 +0100

    Final type definitions and @neo4j/graphql@2.0.0-alpha.1

--- src/typeDefs.ts
+++ src/typeDefs.ts
@@ -3,10 +3,27 @@ import { gql } from "apollo-server";
 const typeDefs = gql`
   type Movie {
     title: String!
+    owners: [User!]!
+      @relationship(type: "OWNS", direction: IN, properties: "Owns")
+    actors: [Actor!]! @relationship(type: "ACTED_IN", direction: IN)
   }
 
   type User {
     name: String!
+    movies: [Movie!]!
+      @relationship(type: "OWNS", direction: OUT, properties: "Owns")
+    moviesCount: Int!
+      @cypher(statement: "MATCH (this)-[:OWNS]->(m:Movie) RETURN count(m)")
+  }
+
+  type Actor {
+    firstName: String!
+    lastName: String!
+    movies: [Movie!]! @relationship(type: "ACTED_IN", direction: OUT)
+  }
+
+  interface Owns {
+    format: String
   }
 `;
 
